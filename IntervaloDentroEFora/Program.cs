1b63c9a31810b09da381fb4ae8f8c3aebf3025a8
Gabriela <gabriela.priaro@gmail.com>
2024-10-29 11:54:26 +0000

    Implementaçao para mostrar quais numeros digitados estao dentro e fora do intervalor de 10 a 20.

--- IntervaloDentroEFora/Program.cs
+++ IntervaloDentroEFora/Program.cs
@@ -1,10 +1,41 @@
-﻿namespace IntervaloDentroEFora
+﻿//obs: Utilizando estrutura Para:
+//Problema "dentro_fora" (adaptado de URI 1072)
+//Leia um valor inteiro N. Este valor será a quantidade de valores inteiros X que serão lidos em seguida.
+//Mostre quantos destes valores X estão dentro do intervalo [10,20] e quantos estão fora do intervalo.
+
+using System.ComponentModel;
+
+namespace IntervaloDentroEFora
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int N, x, contDentro, contFora;
+
+            contDentro = 0;
+            contFora = 0;
+
+            Console.Write("Quantos numeros serão digitados? ");
+            N = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < N; i++)
+            {
+                Console.Write("Digite um numero: ");
+                x = int.Parse(Console.ReadLine());
+
+                if (x >= 10 && x <= 20)
+                {
+                    contDentro++;
+                }
+                else
+                {
+                    contFora++;
+                }
+            }
+
+            Console.WriteLine($"{contDentro} DENTRO");
+            Console.WriteLine($"{contFora} FORA ");
         }
     }
 }
