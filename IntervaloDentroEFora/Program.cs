250579471b54fe3edbafad26baff3dad69af87c5
Gabriela <gabriela.priaro@gmail.com>
2024-10-29 11:17:18 +0000

    Adicionar arquivos de projeto.

---
+++ IntervaloDentroEFora/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace IntervaloDentroEFora
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
